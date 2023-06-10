ebbd9256417e0e2b13fd986e8f4994dbb56454dc
sonkarsagar <110252895+sonkarsagar@users.noreply.github.com>
2023-06-11 00:47:09 +0530

    Add files via upload

---
+++ Full stack Expense App/BACKEND/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const sequelize = require("./util/database");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const user = require("./models/user");
+const expense = require("./models/expense");
+const Orders = require("./models/orders");
+const Leaderboard=require('./models/leaderboard')
+
+const loginRoute=require('./router/loginRoute')
+const userRoute=require('./router/userRoute')
+const expenseRoute=require('./router/expenseRoute')
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(loginRoute)
+
+app.use(userRoute)
+
+app.use(expenseRoute)
+
+user.hasMany(expense);
+expense.belongsTo(user);
+
+user.hasMany(Orders);
+Orders.belongsTo(user);
+
+Leaderboard.belongsTo(user);
+
+sequelize
+  .sync()
+  // .sync({force: true})
+  .then((res) => {
+    const hostname = "127.0.0.1";
+    const port = 3000;
+    app.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
